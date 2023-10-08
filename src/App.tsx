2eb348cf643b218389852ead5aa29c1a2e465ec3
kjubik <wojt.kubicki@gmail.com>
2023-10-09 00:21:52 +0200

    track user state + add log out

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import GoogleSignIn from './components/Auth/GoogleSignIn'
-
-// Import the functions you need from the SDKs you need
+import UserStatus from './components/Auth/UserStatus';
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDr3WFxCxZQKms1Jy-hGjW2x6B7oPiDNAw",
     authDomain: "wemeet-fd684.firebaseapp.com",
@@ -15,14 +12,13 @@ const firebaseConfig = {
     appId: "1:105785457128:web:1027db414ae82d6e430c5d"
 };
 
-// Initialize Firebase
 initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <h1>WeMeet</h1>
-      <GoogleSignIn />
+    <div>
+      <h1 className='text-3xl font-bold'>WeMeet</h1>
+      <UserStatus />
     </div>
   )
 }
