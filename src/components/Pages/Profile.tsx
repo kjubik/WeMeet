47025c9db881f1a3e2f7788a661b2918a228339c
kjubik <wojt.kubicki@gmail.com>
2023-10-09 23:31:11 +0200

    fetch docs from collection

--- src/components/Pages/Profile.tsx
+++ src/components/Pages/Profile.tsx
@@ -1,8 +1,20 @@
+import { getAllUserDocumentIds } from "../Database/ShowData";
+
+async function fetchUserDocumentIds() {
+    try {
+        const documentIds = await getAllUserDocumentIds();
+        console.log('User Document IDs:', documentIds);
+        // You can now use the document IDs as needed
+    } catch (error) {
+        console.error('Error fetching user document IDs:', error);
+    }
+}
 
 function Profile() {
     return (
         <div>
             <h1 className="font-bold">Profile Page</h1>
+            <button onClick={fetchUserDocumentIds}>fetch user ids</button>
         </div>
     );
 }
