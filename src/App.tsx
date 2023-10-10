52e911b6afd9a464dd11b25763dc9d32697cbbf9
kjubik <wojt.kubicki@gmail.com>
2023-10-10 15:10:08 +0200

    ask newly registered users for username

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Navbar from './components/Layouts/Navbar';
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDr3WFxCxZQKms1Jy-hGjW2x6B7oPiDNAw",
@@ -18,6 +19,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 function App() {
--- src/components/Pages/UsernameForm.tsx
+++ src/components/Pages/UsernameForm.tsx
@@ -1,10 +1,59 @@
+import React, { FC, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../App';
+import { addDocToCollection } from '../Database/firestoreUtils';
 
-function UsernameForm() {
-    return (
-        <div>
-            <h1 className="font-bold">You don't have a username yet!</h1>
-        </div>
-    );
+interface UserData {
+  username: string;
+  email: string;
 }
+
+const UsernameForm: FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const currentUser = auth.currentUser;
+
+  const navigate = useNavigate();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (currentUser) {
+      const userData: UserData = {
+        username: username,
+        email: currentUser.email || '',
+      };
+
+      try {
+        await addDocToCollection('users', currentUser.uid, userData);
+        navigate('/profile')
+      } catch (error) {
+        // Handle error
+        console.error('Error adding document: ', error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="font-bold">You don't have a username yet!</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          className="border-2 border-black px-2 py-1"
+          value={username}
+          onChange={handleInputChange}
+        />
+        <button type="submit" className="border-2 border-black px-2 py-1">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default UsernameForm;
