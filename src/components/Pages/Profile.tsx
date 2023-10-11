5bc08d5edf9337d4f172b6e7c14745c4579bfc85
kjubik <wojt.kubicki@gmail.com>
2023-10-12 01:43:05 +0200

    extend add meeting feat

--- src/components/Pages/Profile.tsx
+++ src/components/Pages/Profile.tsx
@@ -1,10 +1,53 @@
+import React, { FC, useState } from 'react';
+import { Meeting, Timeframe } from "../Database/firestoreTypes";
 import { createMeeting } from "../Database/firestoreUtils";
+import { Timestamp } from 'firebase/firestore';
+import { auth } from '../../App';
 
 function Profile() {
+    const currentDate = new Date();
+    const firestoreTimestamp = Timestamp.fromDate(currentDate);
+
+    // Ensure you handle the case where auth.currentUser is not defined
+    const uid = auth.currentUser ? auth.currentUser.uid : '';
+
+    const [meetingData, setMeetingData] = useState<Meeting>({
+        name: '',
+        owner: uid,
+        members: [uid],
+        createdOn: firestoreTimestamp,
+        isDeleted: false,
+        isArchived: false,
+        timeframes: [],
+    });
+
+    const handleMeetingDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMeetingData({
+            ...meetingData,
+            name: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        createMeeting(meetingData);
+    };
+
     return (
         <div>
             <h1 className="font-bold">Profile Page</h1>
-            <button onClick={() => createMeeting({ name: "A party", owner: 'user/userID', members: ['users/userID']})} className="border-2 border-black px-2 py-1">Create a meeting</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={meetingData.name}
+                    onChange={handleMeetingDataChange}
+                    placeholder="title"
+                    className='border-2 border-black px-2 py-1'
+                />
+                <button type="submit" className="border-2 border-black px-2 py-1">
+                    Create meeting
+                </button>
+            </form>
         </div>
     );
 }
