47fe7674ca77966daeee7e5bb3e2244ed9e5047f
kjubik <wojt.kubicki@gmail.com>
2023-10-12 01:42:44 +0200

    add Timeframe type + modify Meeting type

--- src/components/Database/firestoreTypes.ts
+++ src/components/Database/firestoreTypes.ts
@@ -1,5 +1,17 @@
+import { Timestamp, DocumentReference } from "firebase/firestore"
+
+export type Timeframe = {
+    start: Timestamp,
+    end: Timestamp,
+    user: DocumentReference,
+}
+
 export type Meeting = {
     name: string,
     owner: string,
     members: string[],
+    createdOn: Timestamp,
+    isDeleted: boolean,
+    isArchived: boolean,
+    timeframes: Timeframe[],
 }
