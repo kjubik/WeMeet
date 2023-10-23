705e998debd4d9bf2be41ca65ecad6bd39dded40
Wojciech <wojt.kubicki@gmail.com>
2023-10-23 09:16:57 +0200

    seperate firebase init from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDr3WFxCxZQKms1Jy-hGjW2x6B7oPiDNAw",
-    authDomain: "wemeet-fd684.firebaseapp.com",
-    projectId: "wemeet-fd684",
-    storageBucket: "wemeet-fd684.appspot.com",
-    messagingSenderId: "105785457128",
-    appId: "1:105785457128:web:1027db414ae82d6e430c5d"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+import { auth, db } from "./firebase.ts";
 
 function App() {
+  console.log(auth);
+  console.log(db);
   return (
     <BrowserRouter>
       <Routes>
