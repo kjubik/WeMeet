29f20aa472d8cadd0e467663ce29f466b470f755
Wojciech <wojt.kubicki@gmail.com>
2023-10-23 08:53:37 +0200

    use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDr3WFxCxZQKms1Jy-hGjW2x6B7oPiDNAw",
@@ -17,7 +18,11 @@ export const db = getFirestore(app);
 
 function App() {
   return (
-    <h1>wemeet</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
