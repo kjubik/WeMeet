6d1edcbb3461e59de3dbdeacb281b05a3490ad75
kjubik <wojt.kubicki@gmail.com>
2023-10-23 18:50:04 +0200

    restructure GoogleSignIn & fix email sign in and registration

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,17 +1,30 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { signInWithGoogle } from "../utilites/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function Regitser() {
 
+    const navigate = useNavigate();
+
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            console.log('Token:', credential?.accessToken, 'User:', result?.user);
+            navigate('/dashboard');
+        }).catch((error) => {
+            console.log("Failed to sign in: ", error);
+        });
+    }
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const auth = getAuth();
-    const navigate = useNavigate();
-
-    const emailRegister = () => {
+    const emailRegister = async () => {
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             console.log("Account created: ", userCredential.user)
@@ -26,13 +39,13 @@ function Regitser() {
         <div className="m-2 flex flex-col items-start gap-4">
             <h1 className="font-semibold text-xl">Create account on WeMeet</h1>
             <button onClick={signInWithGoogle} className="text-blue-500 font-semibold">Register with Google</button>
-            <form onSubmit={emailRegister} className="flex flex-col items-start gap-2">
+            <div className="flex flex-col items-start gap-2">
                 <label htmlFor="email">Email</label>
                 <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" className="text-blue-500 font-semibold">Sign In</button>
-            </form>
+                <button type="button" onClick={emailRegister} className="text-blue-500 font-semibold">Create account</button>
+            </div>
             <p>Already have an account? <a href="/signin" className="text-blue-500 font-semibold">Sign In</a></p>
         </div>
     )
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,12 +1,25 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { signInWithGoogle } from "../utilites/auth";
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function SignIn() {
 
-    const auth = getAuth();
     const navigate = useNavigate();
+
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            console.log('Token:', credential?.accessToken, 'User:', result?.user);
+            navigate('/dashboard');
+        }).catch((error) => {
+            console.log("Failed to sign in: ", error);
+        });
+    }
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -26,13 +39,13 @@ function SignIn() {
         <div className="m-2 flex flex-col items-start gap-4">
             <h1 className="font-semibold text-xl">Sign In to WeMeet</h1>
             <button onClick={signInWithGoogle} className="text-blue-500 font-semibold">Sign In with Google</button>
-            <form onSubmit={emailSignIn} className="flex flex-col items-start gap-2">
+            <div className="flex flex-col items-start gap-2">
                 <label htmlFor="email">Email</label>
                 <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" className="text-blue-500 font-semibold">Sign In</button>
-            </form>
+                <button type="button" onClick={emailSignIn} className="text-blue-500 font-semibold">Sign In</button>
+            </div>
             <p>Don't have an account yet? <a href="/register" className="text-blue-500 font-semibold">Create account</a></p>
         </div>
     )
