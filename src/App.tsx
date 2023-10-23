d85bb22f38a91bea35ff7eaaf91ae9289504e080
kjubik <wojt.kubicki@gmail.com>
2023-10-23 19:11:34 +0200

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,26 @@ function App() {
   console.log(auth);
   console.log(db);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div><a href="/signin">Sign In</a><br/><a href="/register">Create account</a></div>} />
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/register" element={<Regitser/>} />
-        <Route path="/profile" element={<Profile/>} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+        <nav>
+          <ul className="p-2 bg-slate-200 flex gap-4">
+            <li><a href="/">Home</a></li>
+            <li><a href="/signin">Sign In</a></li>
+            <li><a href="/register">Create Account</a></li>
+            <li><a href="/profile">Profile</a></li>
+          </ul>
+        </nav>
+        <main className="p-4">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div><a href="/signin">Sign In</a><br/><a href="/register">Create account</a></div>} />
+            <Route path="/signin" element={<SignIn/>} />
+            <Route path="/register" element={<Regitser/>} />
+            <Route path="/profile" element={<Profile/>} />
+          </Routes>
+        </BrowserRouter>
+        </main>
+    </div>
   )
 }
 
