77a09d62e623c3b27f3155414e291d9150da91fb
Wojciech <wojt.kubicki@gmail.com>
2023-11-02 02:39:35 +0100

    chore: modify Profile page layout

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,13 @@
 import { getAuth, signOut } from "@firebase/auth";
 import PrimaryButton from "../components/PrimaryButton";
+import { useEffect, useState } from "react";
+import { getUser } from "../firebase/firestore";
+import { User } from "../firebase/types";
 
 function Profile() {
 
     const auth = getAuth();
+    const [userData, setUserData] = useState<User>(); // State to store user's data
 
     const handleLogout = () => {
         signOut(auth)
@@ -16,11 +20,24 @@ function Profile() {
             });
     };
 
+    useEffect(() => {
+        if (auth.currentUser) {
+            getUser(auth.currentUser.uid).then((user) => {
+                setUserData(user);
+            });
+        }
+    }, []);
+
 
     return (
-        <div>
-            <p>Linked email: {auth.currentUser?.email}</p>
-            <PrimaryButton buttonText="Sign out" onClick={handleLogout} />
+        <div className="w-full">
+            <div className="mx-auto max-w-sm flex flex-col items-center">
+                <h1 className="text-3xl font-bold text-center">{userData?.name}</h1>
+                <h2 className="mt-1 text-neutral-500">{userData?.email}</h2>
+                <div className="mt-8">
+                    <PrimaryButton buttonText="Sign Out" onClick={handleLogout} />
+                </div>
+            </div>
         </div>
     );
 };
