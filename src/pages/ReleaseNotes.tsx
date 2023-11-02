a07cca88120ac6eedb26ef4fce2e650ed3da2bd5
kjubik <wojt.kubicki@gmail.com>
2023-11-02 23:56:35 +0100

    refactor: event creation message

--- src/pages/ReleaseNotes.tsx
+++ src/pages/ReleaseNotes.tsx
@@ -4,7 +4,7 @@ function ReleaseNotes() {
     const updates = [
         {
             title: 'Added events creation',
-            description: "We've added a new subpage for managing your events. 🎉 More features for event creation and modification are planned for future releases.",
+            description: "We've added a new subpage for creating events! 🎉 For the time being you can add a custom title and description, but there is more to come soon.",
             date: '2023-10-31',
             isBugFix: false,
         },
@@ -23,7 +23,7 @@ function ReleaseNotes() {
                 <div className="max-w-sm mb-6">
                     <hr className="my-4"/>
                     <h2 className="text-xl font-semibold mb-1">{update.title}</h2>
-                    <h3 className="text-gray-700 font-thin mb-2">{update.date} | {update.isBugFix ? '🪳 Bug fix' : '✨Update'}</h3>
+                    <h3 className="text-gray-700 font-thin mb-2">{update.date} | {update.isBugFix ? '🛠️ Fix' : '✨Update'}</h3>
                     <p>{update.description}</p>
                 </div>
             ))}
