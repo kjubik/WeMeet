969468612857673e62450f89444044047fee1430
Wojciech <wojt.kubicki@gmail.com>
2023-11-13 09:00:16 +0100

    feat: update custom types to fit new db schema

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -3,6 +3,7 @@ import { User, Event } from './types';
 import { collection, doc, getDocs, getDoc, addDoc, runTransaction, setDoc, deleteDoc, updateDoc, query, where } from 'firebase/firestore';
 
 
+
 export const getAllUsers = async (): Promise<User[]> => {
   const querySnapshot = await getDocs(collection(db, "user"));
   return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as User));
@@ -25,96 +26,96 @@ export const createUser = async (user: User) => {
 }
 
 
-export const searchUserByUsername = async (username: string): Promise<string | null> => {
-  try {
-    const usersCollection = collection(db, "user");
-    const q = query(usersCollection, where("username", "==", username));
-    const querySnapshot = await getDocs(q);
+// export const searchUserByUsername = async (username: string): Promise<string | null> => {
+//   try {
+//     const usersCollection = collection(db, "user");
+//     const q = query(usersCollection, where("username", "==", username));
+//     const querySnapshot = await getDocs(q);
 
-    if (querySnapshot.empty) {
-      return null;
-    }
+//     if (querySnapshot.empty) {
+//       return null;
+//     }
     
-    const userId = querySnapshot.docs[0].id;
-    return userId;
-  }
-  catch (error) {
-    throw error;
-  }
-}
+//     const userId = querySnapshot.docs[0].id;
+//     return userId;
+//   }
+//   catch (error) {
+//     throw error;
+//   }
+// }
 
 
-export const getEvents = async (): Promise<Event[]> => {
-  const querySnapshot = await getDocs(collection(db, "events"));
-  return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Event));
-}
+// export const getEvents = async (): Promise<Event[]> => {
+//   const querySnapshot = await getDocs(collection(db, "events"));
+//   return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Event));
+// }
 
 
-export const getUserEvents = async (userId: string): Promise<Event[]> => {
-  const querySnapshot = await getDocs(collection(db, `users/${userId}/events`));
-  return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Event));
+// export const getUserEvents = async (userId: string): Promise<Event[]> => {
+//   const querySnapshot = await getDocs(collection(db, `users/${userId}/events`));
+//   return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Event));
   
-}
+// }
 
 
-export const createEvent = async (event: Omit<Event, 'id'>, userId: string | undefined) => {
-  try {
-    await runTransaction(db, async (_transaction) => {
-      const newEvent = await addDoc(collection(db, "events"), event);
+// export const createEvent = async (event: Omit<Event, 'id'>, userId: string | undefined) => {
+//   try {
+//     await runTransaction(db, async (_transaction) => {
+//       const newEvent = await addDoc(collection(db, "events"), event);
 
-      await setDoc(doc(db, `users/${userId}/events`, newEvent.id), event);
+//       await setDoc(doc(db, `users/${userId}/events`, newEvent.id), event);
 
-      for (const inviteeId of event.invitees) {
-        await inviteUserToEvent(inviteeId, newEvent.id);
-      }
-    });
-    console.log("Event created successfully!");
-  }
-  catch (error) {
-    console.error("Error creating the event:", error);
-    throw error;
-  }
-}
-
-
-export const deleteEvent = async (eventId: string, userId: string | undefined) => {
-  try {
-    await runTransaction(db, async (_transaction) => {
-      await deleteDoc(doc(db, `users/${userId}/events`, eventId));
-      await deleteDoc(doc(db, "events", eventId));
-    });
-  }
-  catch (error) {
-    console.error("Error deleting the event:", error);
-    throw error;
-  }
-}
+//       for (const inviteeId of event.invitees) {
+//         await inviteUserToEvent(inviteeId, newEvent.id);
+//       }
+//     });
+//     console.log("Event created successfully!");
+//   }
+//   catch (error) {
+//     console.error("Error creating the event:", error);
+//     throw error;
+//   }
+// }
 
 
-export const inviteUserToEvent = async (userId: string, eventId: string) => {
-  try {
-    const docRef = doc(db, `users/${userId}`);
-    const userDoc = await getDoc(docRef);
-    const user = userDoc.data() as User;
-    await updateDoc(docRef, {
-      eventInvites: [...user.eventInvites, eventId]
-    });
-  }
-  catch (error) {
-    console.error("Error inviting to the event:", error);
-    throw error;
-  }
-}
+// export const deleteEvent = async (eventId: string, userId: string | undefined) => {
+//   try {
+//     await runTransaction(db, async (_transaction) => {
+//       await deleteDoc(doc(db, `users/${userId}/events`, eventId));
+//       await deleteDoc(doc(db, "events", eventId));
+//     });
+//   }
+//   catch (error) {
+//     console.error("Error deleting the event:", error);
+//     throw error;
+//   }
+// }
 
 
-export const inviteUsersToEventSQL = async (eventId: string, userId: string) => {
-  try {
-    await runTransaction(db, async (_transaction) => {
-      await addDoc(collection(db, "mapEventsUsers"), {eventId, userId});
-    });
-    console.log("Event created successfully!");
-  } catch (error) {
-    console.error("Error creating the event:", error);
-    throw error;
-  }
-}
+// export const inviteUserToEvent = async (userId: string, eventId: string) => {
+//   try {
+//     const docRef = doc(db, `users/${userId}`);
+//     const userDoc = await getDoc(docRef);
+//     const user = userDoc.data() as User;
+//     await updateDoc(docRef, {
+//       eventInvites: [...user.eventInvites, eventId]
+//     });
+//   }
+//   catch (error) {
+//     console.error("Error inviting to the event:", error);
+//     throw error;
+//   }
+// }
+
+
+// export const inviteUsersToEventSQL = async (eventId: string, userId: string) => {
+//   try {
+//     await runTransaction(db, async (_transaction) => {
+//       await addDoc(collection(db, "mapEventsUsers"), {eventId, userId});
+//     });
+//     console.log("Event created successfully!");
+//   } catch (error) {
+//     console.error("Error creating the event:", error);
+//     throw error;
+//   }
+// }
--- src/firebase/types.ts
+++ src/firebase/types.ts
@@ -1,27 +1,51 @@
 
 export interface User {
     id?: string;         // This should be the UID from Firebase Authentication
+    isDeleted: boolean;
+    creationDate: string;
     username: string;
-    name: string;
+    displayName: string;
     email: string;
-    events: string[];
-    eventInvites: string[];
+    status: "enabled" | "disabled";
 }
 
 export interface Event {
-    id: string;
+    id?: string;
     isDeleted: boolean;
+    creationDate: string;
     title: string;
+    ownerId: string;
     description: string;
-    date: string;
-    time: string;
-    organizer: string | undefined;
-    participants: (string | undefined)[];
-    invitees: string[];
+    location: string;       // this should allow for more complexity (online with link what platform, in-person location, etc.)
+    status: "upcoming" | "ongoing" | "completed" | "cancelled";
+    tags: string[];
+    visibility: "public" | "private";
+    createdBy: string;
+    startDate: string;
+    endDate: string;
 }
 
-export interface mapEventUser {
-    id: string;
+export interface Participation {
+    id?: string;
     userId: string;
     eventId: string;
+}
+
+export interface Invitation {
+    id?: string;
+    isDeleted: boolean;
+    creationDate: string;
+    recipientId: string;
+    eventId: string;
+    senderId: string;
+    sendDate: string;
+    expirationDate: string;
+}
+
+export interface Timeslot {
+    id?: string;
+    userId: string;
+    eventId: string;
+    startTime: string;
+    endTime: string;
 }
