00378c9173d29fdf6b434e4dde2c76f97daf3d6d
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-03-06 12:50:07 +0100

    feat: allow user to sign up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,28 @@
-<script>
-    import {app} from '../firebaseConfig';
+<script lang="ts">
+    import { auth } from '../firebaseConfig';
+    import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-    $: console.log(app);
+    let email = '';
+    let password = '';
 
+    const handleSignUp = (email: string, password: string) => {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log('User created', user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        });
+    }
 
 </script>
 
 <h1>Welcome to WeMeet</h1>
+<form>
+    <input type="email" placeholder="Email" bind:value={email}>
+    <input type="password" placeholder="Password" bind:value={password}>
+    <button type="button" on:click={() => handleSignUp(email, password)}>Sign Up</button>
+</form>
