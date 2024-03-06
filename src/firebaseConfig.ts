59e86098f742e74202706a8f8a754e2b5f6a84ba
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-03-06 12:33:31 +0100

    feat: init firebase app

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALZ7-2HyK7RGe3jnQG_9mRBksw-meRyPM",
+  authDomain: "wemeet-calendar.firebaseapp.com",
+  projectId: "wemeet-calendar",
+  storageBucket: "wemeet-calendar.appspot.com",
+  messagingSenderId: "967912408708",
+  appId: "1:967912408708:web:f10952d5a60d285c1be4d1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
