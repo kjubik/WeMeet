51937712649c593bab9e19fefc5fc88019d3ad66
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-03-08 03:06:35 +0100

    feat: track auth state using onAuthStateChanged()

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { auth } from "../firebaseConfig";
+    import { onAuthStateChanged } from "firebase/auth";
+    import { user } from "../stores";
+
+    onAuthStateChanged(auth, (currentUser) => {
+        user.set(currentUser);
+    });
+</script>
+
+<slot />
