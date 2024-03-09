f8ffa3320412f9399d451359058a890d56b594ab
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-03-09 02:03:32 +0100

    feat: create barebones page for event creation

---
+++ src/routes/app/event/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+    let eventData = {
+        title: '',
+        description: '',
+        duration: 0,
+        start: '',
+        end: ''
+    }
+
+    $: console.log(eventData);
+
+    function handleCreateEvent() {
+        console.log('Creating event', eventData);
+    }
+
+</script>
+
+<h1>Create a new event</h1>
+<form>
+    <input type="text" placeholder="Title" bind:value={eventData.title}>
+    <input type="text" placeholder="Description" bind:value={eventData.description}>
+    <input type="number" placeholder="Duration in seconds" bind:value={eventData.duration}>
+    <input type="datetime-local" bind:value={eventData.start}>
+    <input type="datetime-local" bind:value={eventData.end}>
+    <button on:click={handleCreateEvent}>Create event</button>
+</form>
--- src/routes/app/profile/+page.svelte
+++ src/routes/app/profile/+page.svelte
@@ -5,3 +5,4 @@
 
 <h1>Profile</h1>
 <p>Welcome to your profile {$user?.email}.</p>
+<a href="/app/event">Create a new event</a>
