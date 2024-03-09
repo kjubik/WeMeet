17ed32517effd14a6b245596e79bc82a911c1762
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-03-09 22:31:53 +0100

    feat(profile): fetch and display basic user details

--- src/routes/app/profile/+page.svelte
+++ src/routes/app/profile/+page.svelte
@@ -1,8 +1,32 @@
 <script lang="ts">
     import { user } from '../../../stores';
+    import { onMount } from 'svelte';
+    import { db } from '../../../firebaseConfig';
+    import { getDoc, doc } from 'firebase/firestore';
+
+    let userData: any;
+    let isLoaded = false;
+
+    onMount(async () => {
+        try {
+            console.log('user', $user?.uid);
+            await getDoc(doc(db, 'user', $user!.uid))
+            .then((docRef) => {
+                userData = docRef.data();
+            })
+            console.log('userData', userData);
+            isLoaded = true;
+        } catch (error) {
+            console.error('Error getting document:', error);
+        }
+    });
 
 </script>
 
-<h1>Profile</h1>
-<p>Welcome to your profile {$user?.email}.</p>
+{#if isLoaded}
+    <h1>Profile</h1>
+    <h2>{userData.displayName}</h2>
+    <h3>@{userData.username}</h3>
+    <h4>✉ {userData.email}</h4>
+{/if}
 <a href="/app/event">Create a new event</a>
