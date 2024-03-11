e4d22617b9f9802386c2f89196e668fa1aafad77
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-03-11 11:51:13 +0100

    feat(profile): upload user pictures to storage

--- src/routes/app/profile/+page.svelte
+++ src/routes/app/profile/+page.svelte
@@ -3,9 +3,16 @@
     import { onMount } from 'svelte';
     import { db } from '../../../firebaseConfig';
     import { getDoc, doc } from 'firebase/firestore';
+    import { storage } from '../../../firebaseConfig';
+    import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
     let userData: any;
     let isLoaded = false;
+    let uploadedFiles: FileList;
+    $: profilePicture = uploadedFiles?.[0];
+    $: console.log('profilePicture', profilePicture);
+    let downloadURL: string;
+    $: console.log('View the uploaded image at', downloadURL)
 
     onMount(async () => {
         try {
@@ -21,6 +28,21 @@
         }
     });
 
+    const handleUploadImage = async () => {
+        const profilePictureRef = ref(storage, `profilePictures/${$user!.uid}`);
+        try {
+            // Upload the profile picture to storage
+            await uploadBytes(profilePictureRef, profilePicture, {contentType: 'image/png'});
+
+            // Get the download URL of the uploaded image
+            downloadURL = await getDownloadURL(profilePictureRef);
+
+            console.log('Profile picture uploaded successfully!');
+        } catch (error) {
+            console.error('Error uploading profile picture:', error);
+        }
+    }
+
 </script>
 
 {#if isLoaded}
@@ -29,4 +51,9 @@
     <h3>@{userData.username}</h3>
     <h4>✉ {userData.email}</h4>
 {/if}
+<input type="file" id="profile-picture" accept="image/png" bind:files={uploadedFiles}>
+<br/>
+<button on:click={handleUploadImage}>Uplaod a profile picture</button>
+<br/>
+<br/>
 <a href="/app/event">Create a new event</a>
