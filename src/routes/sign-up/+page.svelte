5c8d33f329f8673c447a30b969e979cdc340561e
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-03-13 14:55:54 +0100

    feat(sign-up): add extensive error handling and user account rollback functionality

--- src/routes/sign-up/+page.svelte
+++ src/routes/sign-up/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import { auth } from '../../firebaseConfig';
-    import { createUserWithEmailAndPassword } from 'firebase/auth';
+    import { createUserWithEmailAndPassword, deleteUser } from 'firebase/auth';
     import { user } from '../../stores';
-    import { query, where, collection, getDocs, addDoc, type QuerySnapshot, doc, setDoc } from 'firebase/firestore';
+    import { query, where, collection, getDocs, type QuerySnapshot, doc, setDoc, deleteDoc } from 'firebase/firestore';
     import { db } from '../../firebaseConfig';
 
     let newUser = {
@@ -14,26 +14,28 @@
     let password = '';
 
     const handleSignUp = async (email: string, password: string) => {
-        try {
-            const takenUsernameQuery = query(collection(db, 'takenUsername'), where('username', '==', newUser.username));
-            const queryResult: QuerySnapshot = await getDocs(takenUsernameQuery);
+        // check if the username provided by the user is available
+        const takenUsernameQuery = query(collection(db, 'takenUsername'), where('username', '==', newUser.username));
+        await getDocs(takenUsernameQuery)
+        .then((queryResult: QuerySnapshot) => {
             if (!queryResult.empty) {
-                alert('Username already in use');
+                alert('Username already in use. Please select another.');
                 return;
             }
-        } catch (error) {
-            console.log('Error: ', error);
-        }
+            console.log('Username available. Proceeding with user sign up.');
+        })
+        .catch((error) => {
+            console.log('Error when checking username availability', error.code);
+            return;
+        });
 
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        // create the user in Firebase Auth
+        await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
             user.set(userCredential.user);
-            console.log('User created', user);
-            const userRef = doc(db, 'user', userCredential.user.uid);
-            await setDoc(userRef, newUser);
-            console.log('Document written');
-            alert('User created');
-        } catch (error: any) {
+            console.log('User created', $user);
+        })
+        .catch((error) => {
             if (error.code == 'auth/email-already-in-use') {
                 alert('Email already in use');
             } else if (error.code == 'auth/invalid-email') {
@@ -43,7 +45,67 @@
             } else {
                 console.error('Error: ', error);
             }
-        }
+            return;
+        })
+
+        // create the user doc in Firestore
+        console.log('Creating user doc in Firestore', $user);
+        const userRef = doc(db, 'user', $user!.uid);
+        await setDoc(userRef, newUser)
+        .then(() => {
+            console.log('Document written');
+        })
+        .catch((error) => {
+            console.error('Failed to create user doc in Firestore: ', error);
+
+            // revert creating user in Firebase Auth
+            deleteUser($user!)
+            .then(() => {
+                console.log('User deleted');
+            })
+            .catch((error) => {
+                console.error('Error deleting user in Firebase Auth: ', error);
+                // TODO: notify support team
+            });
+
+            return;
+        });
+
+        // create the username doc in Firestore
+        console.log('Creating username doc in Firestore', $user);
+        const usernameRef = doc(db, 'takenUsername', $user!.uid);
+        await setDoc(usernameRef, { username: newUser.username })
+        .then(() => {
+            console.log('Document written');
+        })
+        .catch((error) => {
+            console.error('Failed to create username doc in Firestore: ', error);
+            
+            // revert creating user in Firebase Auth
+            deleteUser($user!)
+            .then(() => {
+                console.log('User deleted in Firebase Auth');
+            })
+            .catch((error) => {
+                console.error('Error deleting user in Firebase Auth: ', error);
+                // TODO: notify support team
+            });
+
+            // revert creating user doc in Firestore
+            deleteDoc(userRef)
+            .then(() => {
+                console.log('User doc deleted in Firestore from "user" collection');
+            })
+            .catch((error: any) => {
+                console.error('Error deleting user doc in Firestore: ', error);
+                // TODO: notify support team
+            });
+
+            alert('An error occured while creating your account. Please try again. If the problem persists, please contact support.');
+            return;
+        });
+
+        alert('Account created!');
     }
 
 </script>
