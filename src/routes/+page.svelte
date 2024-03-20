4f291e05bfda0a03bb209ee03ab67690f6383ec5
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-03-20 05:19:18 +0100

    feat(events): add subpage for viewing events list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Welcome to WeMeet</h1>
 <a href="/sign-in">Sign In</a>
 <a href="/sign-up">Sign Up</a>
+<a href="/app/events">My Events</a>
 <a href="/app/profile">Go to Profile</a>
--- src/routes/app/events/+page.svelte
+++ src/routes/app/events/+page.svelte
@@ -1,1 +1,41 @@
-My Events
+<script lang="ts">
+    
+    let upcomingEvents: any[] = [];
+
+    let showOngoing = false;
+    let showPast = false;
+
+    let selectedTab: 'upcoming' | 'ongoing' | 'past' = 'upcoming';
+
+</script>
+
+<h1>My Events</h1>
+<div>
+    <button>Upcoming</button>
+    <button disabled={!showOngoing}>Ongoing</button>
+    <button disabled={!showPast}>Past</button>
+</div>
+
+<div>
+{#if selectedTab == 'upcoming'}
+    
+    {#if upcomingEvents.length == 0}
+    
+        <p>No upcoming events</p>
+    
+    {:else}
+
+        <ul>
+            {#each upcomingEvents as event}
+            <li>{event.name}</li>
+            {/each}
+        </ul>
+
+    {/if}
+
+{:else if selectedTab == 'ongoing'}
+
+{:else if selectedTab == 'past'}
+
+{/if}
+</div>
